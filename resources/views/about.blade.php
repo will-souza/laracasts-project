5c959e49d4895b638a71218ddef2ec28dd47a2ca
William Souza <uiru42@gmail.com>
2025-02-02 19:41:21 -0300

    Day 2 - Your First Route and View

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laracasts</title>
+    <title>About Page</title>
 </head>
 <body>
-    <h1>Hello, World!</h1>
+    <h1>Hello from About Page.</h1>
 </body>
 </html>
