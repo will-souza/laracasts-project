9852b9a5e2b63b298c28e3b5f782b5b56fc6fc78
William Souza <uiru42@gmail.com>
2025-02-02 19:54:36 -0300

    Day 3 - Create a Layout File Using Laravel Components

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,6 +7,12 @@
     <title>Laracasts</title>
 </head>
 <body>
-    <h1>Hello, World!</h1>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+    </nav>
+
+    {{ $slot }}
 </body>
 </html>
